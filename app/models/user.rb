4459809182b68a354f9d98a9d8edb549bd116cd8
AustinRhoads <austinrrhoads@ymail.com>
2020-08-03 11:49:22 -0500

    created user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+
+class User < ActiveRecord::Base
+    extend Slugifiable::ClassMethods
+    include Slugifiable::InstanceMethods
+    has_secure_password
+    has_many :sightings
+    validates_presence_of :username, :email, :password
+    scope :ci_find, lambda { |attribute, value| where("lower(#{attribute}) = ?", value.downcase).first }
+end
