1994729289c3309f3c1c685d77cfeaf0933c6edf
AustinRhoads <austinrrhoads@ymail.com>
2020-09-30 14:00:56 -0500

    show files updated

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -4,4 +4,17 @@ class Encounter < ActiveRecord::Base
 
     validates_presence_of :description, :name
     validates_uniqueness_of :description
+
+    def kind_ordinalize
+        if (11..13).include?(self.kind % 100)
+          "#{self.kind}th"
+        else
+          case self.kind % 10
+            when 1; "#{self.kind}st"
+            when 2; "#{self.kind}nd"
+            when 3; "#{self.kind}rd"
+            else    "#{self.kind}th"
+          end
+        end
+      end
 end
